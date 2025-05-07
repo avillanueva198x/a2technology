4c79d0e1bb7c1ea904e8186eb64e08dc5fa61fee
Adolfo Villanueva <avillanueva198x@gmail.com>
2025-05-07 10:57:09 -0500

    primer despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/a2tech-digital-forge/',
+  base: mode === 'production' ? '/a2tech-digital-forge/' : '/',
   server: {
     host: "::",
     port: 8080,
